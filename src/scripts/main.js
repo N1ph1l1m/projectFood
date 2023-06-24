ec02bd25be1ce0e48679e6d10548f07db7157272
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-06-24 22:59:05 +0300

    Refactor code 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -430,8 +430,9 @@ window.addEventListener("DOMContentLoaded", () => {
   //     closeModal();
   //   },1500);
   // }
-  fetch('/src/db.json')
+fetch('db.json',)
   .then(data => data.json())
-  .then (res => console.log(res));
+  .then (res => console.log(res))
+ 
 
 });
