bf55c82b80ba6b51150575982b0def3ae97c3e2d
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-06-24 22:19:33 +0300

    Project-food work js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -105,56 +105,56 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //Timer
 
-  const deadLine = "2023-06-17";
+  // const deadLine = "2023-06-17";
 
-  function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-      minutes = Math.floor((t / 1000 / 60) % 60),
-      seconds = Math.floor((t / 1000) % 60);
+  // function getTimeRemaining(endtime) {
+  //   const t = Date.parse(endtime) - Date.parse(new Date()),
+  //     days = Math.floor(t / (1000 * 60 * 60 * 24)),
+  //     hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+  //     minutes = Math.floor((t / 1000 / 60) % 60),
+  //     seconds = Math.floor((t / 1000) % 60);
 
-    return {
-      total: t,
-      days: days,
-      hours: hours,
-      minutes: minutes,
-      seconds: seconds,
-    };
-  }
-  function getZero(num) {
-    if (num >= 0 && num < 10) {
-      return `0${num}`;
-    } else {
-      return num;
-    }
-  }
+  //   return {
+  //     total: t,
+  //     days: days,
+  //     hours: hours,
+  //     minutes: minutes,
+  //     seconds: seconds,
+  //   };
+  // }
+  // function getZero(num) {
+  //   if (num >= 0 && num < 10) {
+  //     return `0${num}`;
+  //   } else {
+  //     return num;
+  //   }
+  // }
 
-  function setClock(selector, endtime) {
-    const timer = document.querySelector(selector),
-      days = timer.querySelector("#days"),
-      hours = timer.querySelector("#hours"),
-      minutes = timer.querySelector("#minutes"),
-      seconds = timer.querySelector("#seconds"),
-      timeInterval = setInterval(updateClock, 1000);
+  // function setClock(selector, endtime) {
+  //   const timer = document.querySelector(selector),
+  //     days = timer.querySelector("#days"),
+  //     hours = timer.querySelector("#hours"),
+  //     minutes = timer.querySelector("#minutes"),
+  //     seconds = timer.querySelector("#seconds"),
+  //     timeInterval = setInterval(updateClock, 1000);
 
-    updateClock();
+  //   updateClock();
 
-    function updateClock() {
-      const t = getTimeRemaining(endtime);
+  //   function updateClock() {
+  //     const t = getTimeRemaining(endtime);
 
-      days.innerHTML = getZero(t.days);
-      hours.innerHTML = getZero(t.hours);
-      minutes.innerHTML = getZero(t.minutes);
-      seconds.innerHTML = getZero(t.seconds);
+  //     days.innerHTML = getZero(t.days);
+  //     hours.innerHTML = getZero(t.hours);
+  //     minutes.innerHTML = getZero(t.minutes);
+  //     seconds.innerHTML = getZero(t.seconds);
 
-      if (t.total <= 0) {
-        clearImmediate(timeInterval);
-      }
-    }
-  }
+  //     if (t.total <= 0) {
+  //       clearImmediate(timeInterval);
+  //     }
+  //   }
+  // }
 
-  setClock(".timer", deadLine);
+  // setClock(".timer", deadLine);
 
   //Modal
 
@@ -237,7 +237,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
   new MenuCard(
-    "/dist/img/vegy.jpg",
+    "img/vegy.jpg",
     "vegy",
     'Меню “Фитнес" ',
     ' Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
@@ -247,7 +247,7 @@ window.addEventListener("DOMContentLoaded", () => {
   ).render();
 
   new MenuCard(
-    "/dist/img/elite.jpg",
+    "img/elite.jpg",
     "elite",
     'Меню “Премиум" ',
     " В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
@@ -257,7 +257,7 @@ window.addEventListener("DOMContentLoaded", () => {
   ).render();
 
   new MenuCard(
-    "/dist/img/post.jpg",
+    "img/post.jpg",
     "post",
     'Меню “Пост" ',
     " Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков",
@@ -269,19 +269,19 @@ window.addEventListener("DOMContentLoaded", () => {
     //Forms
 
 
-    // const forms = document.querySelectorAll("form");
+    const forms = document.querySelectorAll("form");
 
-    // const message = { 
-    //   loading: "img/form/spinner.svg",
-    //   success: "Thanks! Soon we are callback you",
-    //   failure: "Wtf...., whats wrong",
-    // };
+    const message = { 
+      loading: "img/form/spinner.svg",
+      success: "Thanks! Soon we are callback you",
+      failure: "Wtf...., whats wrong",
+    };
 
-    // forms.forEach(item => {
-    //   postData(item);
-    // });
+    forms.forEach(item => {
+      postData(item);
+    });
     //post data first version
-    /*
+    
     function postData(form) {
       form.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -315,7 +315,7 @@ window.addEventListener("DOMContentLoaded", () => {
         });
       });
     }
-    */
+    
     //form json  
     /*
     function postData(form) {
@@ -407,27 +407,31 @@ window.addEventListener("DOMContentLoaded", () => {
   }
   */
 
-  function showThanksModal(message){
-    const prevModalDialog = document.querySelector(".modal__dialog ");
+  // function showThanksModal(message){
+  //   const prevModalDialog = document.querySelector(".modal__dialog ");
 
-    prevModalDialog.classList.add('hide');
-    openModal();
+  //   prevModalDialog.classList.add('hide');
+  //   openModal();
 
-    const thankModal = document.createElement('div');
-    thankModal.classList.add('modal__dialog');
-    thankModal.innerHTML = `
-      <div class="modal__content">
-        <div class="modal__close" data-close>x</div>
-        <div class="modal__title">${message}</div>
-      </div>
-    `;
+  //   const thankModal = document.createElement('div');
+  //   thankModal.classList.add('modal__dialog');
+  //   thankModal.innerHTML = `
+  //     <div class="modal__content">
+  //       <div class="modal__close" data-close>x</div>
+  //       <div class="modal__title">${message}</div>
+  //     </div>
+  //   `;
 
-    document.querySelector('.modal').append(thankModal);
-    setTimeout(() => {
-      thankModal.remove();
-      prevModalDialog.classList.add('show');
-      prevModalDialog.classList.remove('hide');
-      closeModal();
-    },1500);
-  }
+  //   document.querySelector('.modal').append(thankModal);
+  //   setTimeout(() => {
+  //     thankModal.remove();
+  //     prevModalDialog.classList.add('show');
+  //     prevModalDialog.classList.remove('hide');
+  //     closeModal();
+  //   },1500);
+  // }
+  fetch('/src/db.json')
+  .then(data => data.json())
+  .then (res => console.log(res));
+
 });
