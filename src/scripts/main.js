ed606ae4fea8126797920e06790763086719f291
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-06-26 23:26:46 +0300

    Add dot's in slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -520,13 +520,14 @@ window.addEventListener("DOMContentLoaded", () => {
     
     //second version
     const slides  = document.querySelectorAll('.offer__slide'),
-    prev  = document.querySelector('.offer__slider-prev'),
-    next = document.querySelector('.offer__slider-next'),
-    total = document.querySelector('#total'),
-    current = document.querySelector('#current'),
-    slidesWrapper = document.querySelector('.offer__slider-wrapper'),
-    slideField = document.querySelector('.offer__slider-inner'),
-    width = window.getComputedStyle(slidesWrapper).width;
+          slider = document.querySelector('.offer__slider'),
+          prev  = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current'),
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slideField = document.querySelector('.offer__slider-inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
 
     let slideIndex = 1;
     let offSet = 0;
@@ -547,6 +548,49 @@ window.addEventListener("DOMContentLoaded", () => {
     slides.forEach(slide =>{
       slide.style.width = width;
     });
+
+    slider.style.position = 'relative';
+    const indicators = document.createElement('ol');
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: 15;
+      display: flex;
+      justify-content: center;
+      margin-right: 15%;
+      margin-left: 15%;
+      list-style: none;`;
+      slider.append(indicators);
+
+      for(let i = 0; i < slides.length; i++){
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i +1 );
+        dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transition: opacity .6s ease;
+        `;
+        indicators.append(dot);
+      }
+
+
+      
+    
+      
+      
     next.addEventListener('click', () =>{
       if(offSet == +width.slice(0,width.length - 2) * (slides.length - 1)){
         offSet = 0;
