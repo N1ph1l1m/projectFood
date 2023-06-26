a0717577474bb664368b8f057ba45b5c1124998e
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-06-26 22:20:44 +0300

    Update slider.Code status - work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -472,10 +472,18 @@ window.addEventListener("DOMContentLoaded", () => {
   
     const slides  = document.querySelectorAll('.offer__slide'),
     prev  = document.querySelector('.offer__slider-prev'),
-    next = document.querySelector('.offer__slider-next');
+    next = document.querySelector('.offer__slider-next'),
+    total = document.querySelector('#total'),
+    current = document.querySelector('#current');
     let slideIndex = 1;
 
     showSlides(slideIndex);
+
+    if(slides.length<10){
+      total.textContent = `0${slides.length}`
+    }else{
+      total.textContent = slides.length;
+    }
 
     function showSlides(n){
       if(n > slides.length){
@@ -489,6 +497,13 @@ window.addEventListener("DOMContentLoaded", () => {
       slides.forEach(item => item.style.display = 'none');
 
       slides[slideIndex - 1].style.display = 'block';
+
+      if(slides.length<10) {
+        current.textContent = `0${slideIndex}`;
+      }else{
+        current.textContent = slideIndex;
+      }
+
     }
 
     function plusSlides(n){
